94305c4f88b1daa6afcacb790be32d601ff1a956
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-06-14 11:58:59 -0400

    Got most of it working
    Co-authored-by: IsabelNoguera9 <isabelnoguera9@gmail.com>

--- server.js
+++ server.js
@@ -18,8 +18,13 @@ app.use(express.json())
 
 
 
-
-
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
 
 
 
@@ -59,27 +64,29 @@ app.post("/jobs", function(req, res) {
 
         $(".jobsearch-SerpJobCard", html).each(function(num, job) {
 
-            returner.push(`<a href="https://indeed.com${$(this).find(".jobtitle").attr("href")}">${$(this).find(".title").text()}</a><br>`)
+            returner.push(`<p class="text-center"><a href="https://indeed.com${$(this).find(".jobtitle").attr("href")}">${$(this).find(".title").text()}</a></p>`)
 
         });
 
         var url = `https://www.monster.com/jobs/search/?q=${job}&where=${location.city} ${location.principalSubdivision} ${location.postcode}`;
         rp(url).then(function(html) {
             $(".card-header > h2.title", html).each(function(num, job) {
-                returner.push(`<a href="${$(this).find("a").attr("href")}">${$(this).text()}</a><br>`)
+                returner.push(`<p class="text-center"><a href="${$(this).find("a").attr("href")}">${$(this).text()}</a></p>`)
 
             })
             var url = `https://ziprecruiter.com/candidate/search/?radius=25&search=${job}&location=${location.city} ${location.principalSubdivision} ${location.postcode}`;
             rp(url).then(function(html) {
                 $(".zr_job_result", html).each(function(num, job) {
-                    returner.push(`<a href="${$(this).find(".job_link").attr("href")}">${$(this).find(".just_job_title").text()}</a><br>`)
+                    returner.push(`<p class="text-center"><a href="${$(this).find(".job_link").attr("href")}">${$(this).find(".just_job_title").text()}</a></p>`)
                 })
-
+                var startString = `<!DOCTYPE html> <html lang="en"> <head> <title>essentialize</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> <style> .navbarcustom { color: #FF8C00; background-color: #191970 } .background { height: 700px; background: linear-gradient(to bottom, #4B0082 0%, #830621 100%) } .nav { text-align: center; margin: 20px 0 0; padding: 10px 0; } .h1 { text-align: center; } .h3 { text-align: center; } .museomoderno { font-family: MuseoModerno } .monospace { font-family: "Lucida Console", Courier, monospace; } } } </style> </head> <body class="background"> <nav class="mx-auto navbar navbar-expand-sm navbarcustom"> <h1 class="monospace">essentialize</h1> <a class="navbar-brand" href="#"> <img src="a.svg" style="width:30px;"> </a> <ul class="navbar-nav "> <li class="nav-item nav"> <a class="mx-auto" style="width: 200px;" class="nav-link monospace mx-auto" href="home.html">Home</a> </li> <li class="nav-item nav "> <a class="mx-auto" style="width: 220px;" class="nav-link monospace mx-auto" href="help.html">Help</a> </li> <li class="nav-item nav"> <a class="mx-auto" style="width: 250px;" class="nav-link monospace mx-auto" href="resources.html">Resources</a> </li> <li class="nav-item nav"> <a class="mx-auto" style="width: 250px;" class="nav-link monospace mx-auto" href="search.html">Search for a job</a> </li> <li class="nav-item dropdown nav "> <div class="dropdown show"> <a class="mx-auto" style="width: 300px;" class="dropdown-toggle monospace" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Creators </a> <div class="dropdown-menu mx-auto" aria-labelledby="dropdownMenuLink"> <a class="dropdown-item " href="dbio.html ">Daniel</a> <a class="dropdown-item " href="ibio.html ">Isabel</a> <a class="dropdown-item " href="tbio.html ">Tyler</a> <a class="dropdown-item " href="ebio.html ">Ellie</a> </div> </div> </li> </ul> </nav><div class="jumbotron">`;
+                var endString = "</div></body></html>"
                 var returnString = "";
                 returner.forEach(function(element) {
                     returnString = returnString + element
                 })
-                res.send(returnString);
+                returnString=shuffle(shuffle(returnString));
+                res.send(startString + returnString + endString);
 
             })
 
